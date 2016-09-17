9d96211569dad20b843c4e4ed95cdb2bd428721f
Madeleine Johnson <mjohnson3038@gmail.com>
2016-09-17 11:14:14 -0700

    Exponent

--- practice.rb
+++ practice.rb
@@ -1,4 +1,10 @@
 def power(base,exp)
+  if exp==1
+    return base
+  else
+    return base * power(base, exp - 1)
+  end
+
 end
 
 def fib(n)
@@ -9,19 +15,19 @@ end
 
 
 # Fibanocci Tests
-raise "fib broke - fib(8)" unless fib(8) == 21
-raise "fib broke - fib(20)" unless fib(20) == 6765
-raise "fib broke - fib(1)" unless fib(1) == 1
-raise "fib broke - fib(2)" unless fib(2) == 1
-puts "passes all fibanocci tests"
-
-# Palindrome Tests
-raise "pal broke - pal('racecar')" unless pal("racecar") == true
-raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
-raise "pal broke - pal('')" unless pal("") == true
-puts "passes all palindrome tests"
-
-
+# raise "fib broke - fib(8)" unless fib(8) == 21
+# raise "fib broke - fib(20)" unless fib(20) == 6765
+# raise "fib broke - fib(1)" unless fib(1) == 1
+# raise "fib broke - fib(2)" unless fib(2) == 1
+# puts "passes all fibanocci tests"
+#
+# # Palindrome Tests
+# raise "pal broke - pal('racecar')" unless pal("racecar") == true
+# raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
+# raise "pal broke - pal('')" unless pal("") == true
+# puts "passes all palindrome tests"
+#
+#
 
 
 # def binary(n)
