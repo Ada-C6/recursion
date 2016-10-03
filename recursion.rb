51cac330488163d3a3bc9d6baeec001c63c68e35
Maya Wang <salome.neptune@gmail.com>
2016-10-03 02:48:16 -0700

    combined files with up-to-date folder content, deleted practice file, add bunny method, kept binary and travel as bonus challenges

--- recursion.rb
+++ recursion.rb
@@ -1,27 +1,13 @@
-#
-# def fact(n)
-#   if n == 1
-#     return 1
-#   else
-#     return  n * fact(n-1)
-#     # n = -1 unattainable base case
-#     # => stack overflow/infinite loop
-#     # activation chain: the actual execution each step in the loop
-#     # record: one call to the method/one level of the bubble
-#     # tail recursion: no computation in recursive line
-#     # common use: true or false
-#     # helper method: call another method
-#   end
-# end
+# Write #1
+def fact(n)
+  if n == 1 || n == 0
+    return 1
+  else
+    return n * fact(n-1)
+  end
+end
 
-# def mystery(n)
-#   if n < 10
-#     return n
-#   else
-#     return(n%10)+mystery(n/10)
-#   end
-# end
-
+# Write #2
 def reverse(s)
   if s.length == 0 || s.length == 1
     return s
@@ -32,6 +18,15 @@ end
 puts reverse("hello")
 # => "olleh"
 
+# Write #3
+def bunny(n)
+  if n == 0
+    return 0
+  end
+  return 2 + bunny(n-1)
+end
+
+# Write #4
 def nested(s)
   if  s.length == 0
     return true
@@ -52,13 +47,108 @@ puts nested("((()))")
 puts nested("())")
 # => false
 
+# fib(n)
+def fib(n)
+  if n == 0 || n == 1
+    return n
+  end
+  return fib( n - 1 ) + fib( n - 2 )
+end
+
+puts fib(4)
+# => 3
+
+# pal(s)
+def pal(s)
+  if  s.length == 0 || s.length == 1
+    return true
+  end
+  if s[0] != s[s.length - 1]
+    return false
+  end
+  return pal(s.slice(1, s.length - 2))
+end
+
+puts pal("racecar")
+# => true
+puts pal("smile")
+# => false
+
+def power(base, exp)
+  if exp == 0
+    return 1
+  end
+  if base == 0 && exp != 0
+    return 0
+  end
+  return base * power(base, exp - 1)
+end
+
+puts power(10, 3)
+# => 1000
+puts power(0, 10)
+# => 0
+puts power(10, 0)
+# => 1
+puts power(0, 0)
+# => 1
+
+def binary(n)
+  if n == 0
+    return ""
+  end
+  nums = binary(n - 1).split(" ")
+  return '0' + nums.join(' 0') + ' 1' + nums.join(' 1') + ' '
+end
+puts binary(0)
+puts binary(1)
+puts binary(2)
+
+# def travel(x,y)
+# end
+
 # Factorial Tests
-# raise "factorial broke - fact(4)" unless fact(4) == 24
-# raise "factorial broke - fact(0)" unless fact(0) == 1
-# puts "passes all factorial tests"
+raise "factorial broke - fact(4)" unless fact(4) == 24
+raise "factorial broke - fact(0)" unless fact(0) == 1
+puts "passes all factorial tests"
 
 # Reverse Tests
+raise "reverse broke - reverse('hello')" unless reverse("hello") == "olleh"
+raise "reverse broke - reverse('a b c')" unless reverse("a b c") == "c b a"
+puts "passes all reverse tests"
 
-# raise "reverse broke - fact('hello')" unless reverse("hello") == "olleh"
+# Bunny Tests
+raise "bunny broke - bunny(0)" unless bunny(0) == 0
+raise "bunny broke - bunny(10)" unless bunny(10) == 20
+puts "passes all bunny tests"
 
-# puts "All test passed"
+# Nested Tests
+raise "nested broke - nested('((()))')" unless nested("((()))") == true
+raise "nested broke - nested('())')" unless nested("())") == false
+puts "passes all nested tests"
+
+
+# Fibanocci Tests
+raise "fib broke - fib(8)" unless fib(8) == 21
+raise "fib broke - fib(20)" unless fib(20) == 6765
+raise "fib broke - fib(1)" unless fib(1) == 1
+raise "fib broke - fib(2)" unless fib(2) == 1
+puts "passes all fibanocci tests"
+
+# Palindrome Tests
+raise "pal broke - pal('racecar')" unless pal("racecar") == true
+raise "pal broke - pal('helloworld')" unless pal("helloworld") == false
+raise "pal broke - pal('')" unless pal("") == true
+puts "passes all palindrome tests"
+
+# Binary Tests
+raise "binary broke - binary(1)" unless binary(1) == "0 1 "
+raise "binary broke - binary(2)" unless binary(2) == "00 01 10 11 "
+raise "binary broke - binary(7)" unless binary(7) == "0000000 0000001 0000010 0000011 0000100 0000101 0000110 0000111 0001000 0001001 0001010 0001011 0001100 0001101 0001110 0001111 0010000 0010001 0010010 0010011 0010100 0010101 0010110 0010111 0011000 0011001 0011010 0011011 0011100 0011101 0011110 0011111 0100000 0100001 0100010 0100011 0100100 0100101 0100110 0100111 0101000 0101001 0101010 0101011 0101100 0101101 0101110 0101111 0110000 0110001 0110010 0110011 0110100 0110101 0110110 0110111 0111000 0111001 0111010 0111011 0111100 0111101 0111110 0111111 1000000 1000001 1000010 1000011 1000100 1000101 1000110 1000111 1001000 1001001 1001010 1001011 1001100 1001101 1001110 1001111 1010000 1010001 1010010 1010011 1010100 1010101 1010110 1010111 1011000 1011001 1011010 1011011 1011100 1011101 1011110 1011111 1100000 1100001 1100010 1100011 1100100 1100101 1100110 1100111 1101000 1101001 1101010 1101011 1101100 1101101 1101110 1101111 1110000 1110001 1110010 1110011 1110100 1110101 1110110 1110111 1111000 1111001 1111010 1111011 1111100 1111101 1111110 1111111 "
+puts "passedd all Binary Tests"
+
+# # Travel Tests
+# raise "travel broke - travel(1,2)" unless travel(1,2) == "ENN NEN NNE "
+# raise "travel broke - travel(2,2)" unless travel(2,2) == "EENN ENEN ENNE NEEN NENE NNEE "
+
+puts "All test passed"
